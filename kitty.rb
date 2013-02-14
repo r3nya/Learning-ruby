e444c2481f429da7e8e0c0ef6952ce89914978b5
Andrew M <me@r3nya.ru>
2013-02-14 22:29:48 +0400

    :smiley_cat: Kitty :heart:

---
+++ kitty.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/ruby
+
+require 'open-uri'
+
+kittens = open('http://placekitten.com/')
+response_status = kittens.status
+response_body = kittens.read[559, 441]
+
+puts response_status
+puts response_body
